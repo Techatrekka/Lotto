244e4e86bc3158f69a3477feb4f27f574ff9f3b6
sean <saslynch12@gmail.com>
2015-03-21 13:28:38 +0000

    initial commit

---
+++ src/com/coderdojo/midwest/Main.java
@@ -0,0 +1,29 @@
+package com.coderdojo.midwest;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * Created by Seantheguy on 21/03/15.
+ */
+public class Main {
+
+    public static void main(String   args[]) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("You have guesses");
+        Random random = new Random();
+        for (int i = 1; i < 6; i++) {
+            int randomNumber = random.nextInt(100);
+            int Guess;
+            System.out.println("Enter a positive Integer");
+            Guess = input.nextInt();
+            System.out.println("You entered " + Guess);
+            if (randomNumber == Guess) {
+                System.out.println("you have guessed correctly");
+
+            } else if (randomNumber != Guess) {
+                System.out.println("You are wrong");
+            }
+        }
+    }
+}
